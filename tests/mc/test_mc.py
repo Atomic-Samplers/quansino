d8f2824339f091123559f466de37e3dd574b66ec
Tom Demeyere <115232841+tomdemeyere@users.noreply.github.com>
2024-11-21 14:52:36 +0000

    Refactored tests, global conftest (#11)

--- tests/mc/test_mc.py
+++ tests/mc/test_mc.py
@@ -7,12 +7,12 @@ import pytest
 from quansino.mc.core import MonteCarlo
 
 
-def test_mc_class(atoms):
+def test_mc_class(bulk_small):
     """Test that the ForceBias class works as expected."""
-    mc = MonteCarlo(atoms, seed=42)
+    mc = MonteCarlo(bulk_small, seed=42)
 
     with pytest.raises(AttributeError):
-        assert MonteCarlo.__rng
+        assert mc.__rng
 
     with pytest.raises(AttributeError):
         assert MonteCarlo.__seed
@@ -20,28 +20,30 @@ def test_mc_class(atoms):
     assert mc._MonteCarlo__seed == 42
     assert mc._MonteCarlo__rng is not None
     assert mc.trajectory is None
-    assert mc.atoms == atoms
+    assert mc.atoms == bulk_small
     assert mc.converged()
 
     mc.nsteps = -1
     assert not mc.converged()
 
 
-def test_mc_logger(atoms, tmp_path):
-    mc = MonteCarlo(atoms, seed=42, logfile="-", loginterval=1)
+def test_mc_logger(bulk_small, tmp_path):
+    mc = MonteCarlo(bulk_small, seed=42, logfile="-", loginterval=1)
 
     assert not Path("-").exists()
     assert mc.logfile is not None
     assert len(mc.observers) == 1
 
-    mc = MonteCarlo(atoms, seed=42, logfile=tmp_path / "mc.log", append_trajectory=True)
+    mc = MonteCarlo(
+        bulk_small, seed=42, logfile=tmp_path / "mc.log", append_trajectory=True
+    )
 
     assert mc.logfile is not None
     assert Path(tmp_path, "mc.log").exists()
 
     logfile_path_str = str(Path(tmp_path, "mc_str.log"))
     mc = MonteCarlo(
-        atoms, seed=42, trajectory=tmp_path / "mc.traj", logfile=logfile_path_str
+        bulk_small, seed=42, trajectory=tmp_path / "mc.traj", logfile=logfile_path_str
     )
 
     assert Path(logfile_path_str).exists()
@@ -50,8 +52,8 @@ def test_mc_logger(atoms, tmp_path):
     assert len(mc.observers) == 2
 
 
-def test_todict(atoms):
-    mc = MonteCarlo(atoms, seed=42)
+def test_todict(bulk_small):
+    mc = MonteCarlo(bulk_small, seed=42)
 
     mc.nsteps = 1234
     assert mc.todict() == {
