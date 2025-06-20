96adcf183efd9a769d2ef24bead0278e7cb07391
Tom Demeyere <115232841+tomdemeyere@users.noreply.github.com>
2025-06-20 23:00:29 +0200

    Complete refactor for official release  (#48)
    * progress
    
    * work in progress
    
    * work in progress
    
    * complete rework
    
    * fix sys.stdout tests
    
    * fix remaining tests due to sys.stdout
    
    * test annotations
    
    * additional tests

--- src/quansino/__init__.py
+++ src/quansino/__init__.py
@@ -4,5 +4,4 @@ from __future__ import annotations
 
 from importlib.metadata import version
 
-# Load the version
 __version__ = version("quansino")
--- tests/utils/test_strings.py
+++ tests/utils/test_strings.py
@@ -4,9 +4,12 @@ from quansino.utils.strings import get_auto_header_format
 
 
 def test_get_auto_header_format():
+    """Test the `get_auto_header_format` function."""
     assert get_auto_header_format("{:10.3f}") == "{:>10s}"
     assert get_auto_header_format("{:4s}") == "{:>4s}"
     assert get_auto_header_format("{:s}") == "{:>10s}"
     assert get_auto_header_format("{:>s}") == "{:>10s}"
     assert get_auto_header_format("{:>10s}") == "{:>10s}"
     assert get_auto_header_format("{:>10}") == "{:>10s}"
+    assert get_auto_header_format("{:>10.3f}{:<10.3f}") == "{:>10s}{:<10s}"
+    assert get_auto_header_format("{:>10.3f}{:>10.3f}") == "{:>10s}{:>10s}"
